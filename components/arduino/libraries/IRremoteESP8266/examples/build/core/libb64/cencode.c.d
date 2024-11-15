25ff27f6cb6c00a79accf034f2f6e8d6eaf28e76
Kulasekaran <kulasekaran@qmaxsys.com>
2024-11-15 19:44:56 +0530

    Added arduino as component - Added IR files - Build successful

---
+++ components/arduino/libraries/IRremoteESP8266/examples/build/core/libb64/cencode.c.d
@@ -0,0 +1,3 @@
+C:\Users\EmbeddedDevelopment\Desktop\MyProjects\IR_proj\CODE\c01230-ir-ble-mesh-ac-controller-gwy\components\arduino\libraries\IRremoteESP8266\examples\build\core\libb64\cencode.c.o: \
+ C:\Users\EmbeddedDevelopment\AppData\Local\Arduino15\packages\esp32\hardware\esp32\2.0.11\cores\esp32\libb64\cencode.c \
+ C:\Users\EmbeddedDevelopment\AppData\Local\Arduino15\packages\esp32\hardware\esp32\2.0.11\cores\esp32\libb64\cencode.h
---
+++ components/arduino/libraries/IRremoteESP8266/examples/build/libraries/IRremoteESP8266/ir_Sherwood.cpp.d
@@ -0,0 +1,4 @@
+C:\Users\EmbeddedDevelopment\Desktop\MyProjects\IR_proj\CODE\c01230-ir-ble-mesh-ac-controller-gwy\components\arduino\libraries\IRremoteESP8266\examples\build\libraries\IRremoteESP8266\ir_Sherwood.cpp.o: \
+ C:\Users\EmbeddedDevelopment\Documents\Arduino\libraries\IRremoteESP8266\src\ir_Sherwood.cpp \
+ C:\Users\EmbeddedDevelopment\Documents\Arduino\libraries\IRremoteESP8266\src\IRsend.h \
+ C:\Users\EmbeddedDevelopment\Documents\Arduino\libraries\IRremoteESP8266\src\IRremoteESP8266.h
